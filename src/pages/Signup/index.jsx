bcbcc215818f37f895a048b5ff0413800716b1c2
Adrián <jasmdp@hotmail.com>
2025-09-09 20:12:05 -0300

    Form done

---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,16 @@
+const Signup = () => {
+
+    return (
+        <div>
+            <h2>Registro</h2>
+
+            <form action="">
+                <label htmlFor=""></label>
+                <input type="text" name="" id="" />
+            </form>
+        </div>
+    )
+
+}
+
+export default Signup
